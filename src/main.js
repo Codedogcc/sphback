a5754f11e8aebfe314fdacfa78c3b3afeaac9355
陈俊沪 <junhuchen@126.com>
2023-06-19 14:16:00 +0800

    置换成现成项目

--- src/main.js
+++ src/main.js
@@ -12,6 +12,8 @@ import App from './App'
 import store from './store'
 import router from './router'
 
+
+
 import '@/icons' // icon
 import '@/permission' // permission control
 
@@ -19,7 +21,8 @@ import '@/permission' // permission control
  * If you don't want to use mock-server
  * you want to use MockJs for mock api
  * you can execute: mockXHR()
- *
+ * 
+ * 
  * Currently MockJs will be used in the production environment,
  * please remove it before going online ! ! !
  */
@@ -34,7 +37,19 @@ Vue.use(ElementUI, { locale })
 // Vue.use(ElementUI)
 
 Vue.config.productionTip = false
+//引入相关API请求接口
+import API from '@/api';
+import CategorySelect from '@/components/CategorySelect';
+//组件实例的原型的原型指向的是Vue.prototype
+//任意组件可以使用API相关的接口
+Vue.prototype.$API =API;
+//注册全局组件
+Vue.component( CategorySelect.name, CategorySelect);
+import HintButton from '@/components/HintButton';
+Vue.component(HintButton.name,HintButton);
 
+//引入v-charts配置文件
+import '@/plugins/vcharts';
 new Vue({
   el: '#app',
   router,
--- src/utils/request.js
+++ src/utils/request.js
@@ -7,10 +7,10 @@ import { getToken } from '@/utils/auth'
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
-  timeout: 30000 // request timeout
+  timeout: 5000 // request timeout
 })
 
-// request interceptor
+//请求拦截器：携带的token字段
 service.interceptors.request.use(
   config => {
     // do something before request is sent
@@ -30,7 +30,7 @@ service.interceptors.request.use(
   }
 )
 
-// response interceptor
+//响应拦截器
 service.interceptors.response.use(
   /**
    * If you want to get http information such as headers or status
@@ -45,12 +45,12 @@ service.interceptors.response.use(
   response => {
     const res = response.data
 
-    // if the custom code is not 20000|200, it is judged as an error.
-    if (res.code !== 20000 && res.code !== 200) {
+    //服务器响应失败在干什么,因为咱们真实服务器返回code  20000也有可能200
+    if (res.code !== 20000 && res.code!=200) {
       Message({
         message: res.message || 'Error',
         type: 'error',
-        duration: 30 * 1000
+        duration: 5 * 1000
       })
 
       // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
@@ -68,6 +68,7 @@ service.interceptors.response.use(
       }
       return Promise.reject(new Error(res.message || 'Error'))
     } else {
+    //服务器相应成功干什么
       return res
     }
   },
